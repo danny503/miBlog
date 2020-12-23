5c8cb5b3099321f48bac73bd7ac019ca4ba01c9a
Daniel Ayala <lopezthedani503@gmail.com>
2020-12-23 17:22:30 -0600

    Listar posts por categorias en la navegación

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Post;
+use App\Models\Category;
 use Illuminate\Http\Request;
 
 class PostController extends Controller
@@ -22,5 +23,14 @@ class PostController extends Controller
                             ->get();
 
         return view('posts.show', compact('post', 'similares'));
+    }
+
+    public function category(Category $category){
+        $posts = Post::where('category_id', $category->id)
+                        ->where('status', 2)
+                        ->latest('id')
+                        ->paginate(6);
+        //dd($posts);
+        return view('posts.category', compact('posts', 'category'));
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -16,6 +16,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', [PostController::class, 'index'])->name('post.index');
 Route::get('/posts/{post}', [PostController::class, 'show'])->name('post.show');
+Route::get('/category/{category}', [PostController::class, 'category'])->name('post.category');
 
 Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
     return view('dashboard');
